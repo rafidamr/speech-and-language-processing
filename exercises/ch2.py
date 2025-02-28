a208fc67db0232b59c8041832b7955f2331004dd
Muhammad Rafid Amrullah <rafid.tijarah@gmail.com>
2025-02-28 07:07:34 +0700

    add static analyser and formatter

--- exercises/ch2.py
+++ exercises/ch2.py
@@ -1,7 +1,22 @@
 import re
 
+
 def eval_alphabet(s: str):
-    return re.fullmatch('^[a-zA-Z]+', s)
+    "the set of all alphabetic strings"
+    return re.fullmatch("^[a-zA-Z]+", s)
+
 
 def eval_lower_alphabet(s: str):
-    return re.fullmatch('^[a-z]+b$', s)
+    """
+    the set of all strings from the alphabet ab such that each a
+    is immediately preceded by and immediately followed by a b
+    """
+    return re.fullmatch("^[a-z]+b$", s)
+
+
+def eval_ab(s: str):
+    """
+    the set of all strings from the alphabet ab such that each a is immedi
+    ately preceded by and immediately followed by a b;
+    """
+    return re.fullmatch("^b(ab)+", s)
