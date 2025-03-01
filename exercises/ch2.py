689bab53122e48de7049c1b994dc9c06862a36f5
Muhammad Rafid Amrullah <rafid.tijarah@gmail.com>
2025-03-01 19:00:39 +0700

    update 2.2

--- exercises/ch2.py
+++ exercises/ch2.py
@@ -18,7 +18,7 @@ def eval_ab(s: str):
     the set of all strings from the alphabet ab such that each a is
     immediately preceded by and immediately followed by a b
     """
-    return re.findall(r"b(ab)+", s)
+    return re.findall(r"b(?:ab)+", s)
 
 
 #   2.2
@@ -29,4 +29,28 @@ def eval_repeated_words(s: str):
     the set of all strings with two consecutive repeated words (e.g., “Hum
     bert Humbert” and “the the” but not “the bug” or “the big bug”)
     """
-    return re.findall(r"(?:^| )([a-zA-Z]+) \1(?:$| )", s)
+    return re.findall(r"(?:^|\s)(\w+) \1(?:$|\s)", s)
+
+
+def eval_lines(s: str):
+    """
+    all strings that start at the beginning of the line with an integer and that
+    end at the end of the line with a word
+    """
+    return re.findall(r"^\d+.*[a-zA-Z]+$", s, flags=re.MULTILINE)
+
+
+def eval_grotto_raven(s: str):
+    """
+    all strings that have both the word grotto and the word raven in them
+    (but not, e.g., words like grottos that merely contain the word grotto)
+    """
+    return re.findall(r".*(?:raven|grotto).*(?:grotto|raven).*", s)
+
+
+def eval_first_word(s: str):
+    """
+    write a pattern that places the first word of an English sentence in a
+    register. Deal with punctuation.
+    """
+    return re.search(r"\b([a-zA-Z]+)\b", s)
--- tests/test_ch2.py
+++ tests/test_ch2.py
@@ -10,3 +10,8 @@ def test_2_1():
 def test_2_2():
     assert ch2.eval_repeated_words("the the")
     assert not ch2.eval_repeated_words("there here")
+    assert ch2.eval_lines("12 word\n333aa")
+    assert not ch2.eval_lines("123\n333")
+    assert ch2.eval_grotto_raven("asdfgrottossravenasdf")
+    assert not ch2.eval_grotto_raven("grottos")
+    assert ch2.eval_first_word("1asdf 'word' asdf a").group() == "word"
